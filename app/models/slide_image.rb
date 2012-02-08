63b2d555381c52f18f3bd0b182e7a33aef305693
danigb <danigb@gmail.com>
2012-02-08 19:33:58 +0100

    Main en slide images

--- app/models/slide_image.rb
+++ app/models/slide_image.rb
@@ -2,6 +2,10 @@ class SlideImage < Pan
 
   RENDER = [:thumb, :position, :body]
 
+  def extra_human_name
+    extra == 'main' ? 'Principal' : extra
+  end
+
   def show_col?(item)
     RENDER.include?(item)
   end
