a81e69d217fbf306c812a86df74735856f364f73
danigb <danigb@gmail.com>
2012-02-15 15:49:35 +0100

    Starting pagination in blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,12 +1,18 @@
 class Blog
-  def posts
-    @posts ||= Post.order('rev_date ASC') #.includes(:image)
+  def posts(page = 1)
+    @posts ||= Post.order('rev_date ASC') #.paginate(page: page, per_page: 5)
   end
 
   def width
     width = posts.each.map { |post| post.width + 10 }.inject { |sum, n| sum + n }
     width = 0 unless width
+    width += 90 if has_previous?
     width < 843 ? 843 : width
+  end
+
+  def has_previous?
+    #posts.current_page < posts.total_pages
+    false
   end
 end
 
--- app/models/selection.rb
+++ app/models/selection.rb
@@ -3,20 +3,8 @@
 class Selection < Pan
   store :settings, accessors: [ :row_position ]
 
-  # Values for extra
-  EXTRA = [:top, :bottom]
-  EXTRA_NAME = 'Posición'
-
-
-  def extra_human_name
-    case self.extra
-      when 'top'
-        'Arriba'
-      when 'bottom'
-        'Abajo'
-      else
-        '¡Error! Escribe a Dani.'
-    end
+  before_save do
+    self.width = 200 if self.file.blank?
   end
 end
 
